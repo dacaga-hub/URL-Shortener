cc64d38fd5f5bed5070e573435cdbded783d07bb
dacaga-hub <daniel.caravacagarcia@gmail.com>
2026-09-15 18:01:13 +0200

    test: add web layer test for URL creation endpoint

---
+++ src/test/java/com/dacaga/shortener/controller/ShortUrlControllerTest.java
@@ -0,0 +1,43 @@
+package com.dacaga.shortener.controller;
+
+import com.dacaga.shortener.domain.ShortUrl;
+import com.dacaga.shortener.service.ShortUrlService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ShortUrlController.class)
+class ShortUrlControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ShortUrlService service;
+
+    @Test
+    void createReturnShortCode() throws Exception{
+        //Preparar: amañar el service
+        ShortUrl fake = new ShortUrl();
+        fake.setUrl("https://google.com");
+        fake.setUrlShort("abc");
+        when(service.create(any())).thenReturn(fake);
+
+        //Actuar + Comprobar
+        mockMvc.perform(post("/api/urls")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"url\": \"https://google.com\"}"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.urlShort").value("abc"))
+                    ;
+    }
+}
